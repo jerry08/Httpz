b96d54ad64a4f66e7d8d2d36b0a87dde4a78bf5d
jerrry08 <jeremyyearwood99@gmail.com>
2023-10-09 08:29:09 -0400

    Add tests

---
+++ Httpz.Tests/QualitySpecs.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Httpz.Tests.TestData;
+using Xunit;
+
+namespace Httpz.Tests;
+
+public class QualitySpecs
+{
+    [Fact]
+    public async Task I_can_get_quality_results_from_an_hls_url()
+    {
+        // Arrange
+        var downloader = new HlsDownloader();
+
+        // Act
+        var results = await downloader.GetQualitiesAsync(HlsUrls.Url1);
+
+        // Assert
+        results.Should().HaveCountGreaterOrEqualTo(1);
+    }
+}
---
+++ Httpz.Tests/TestData/HlsUrls.cs
@@ -0,0 +1,7 @@
+﻿namespace Httpz.Tests.TestData;
+
+internal static class HlsUrls
+{
+    public const string Url1 =
+        "http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8";
+}
