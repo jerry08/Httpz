dae90420474892a61f949fc7cc56b90b78bc36f7
jerrry08 <jeremyyearwood99@gmail.com>
2023-10-09 08:29:15 -0400

    Cleanup sample

---
+++ Httpz.Demo.Cli/Program.cs
@@ -0,0 +1,26 @@
+﻿using Httpz;
+using Spectre.Console;
+
+namespace ConsoleApp1;
+
+internal static class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Httpz Demo";
+
+        var url = "http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8";
+
+        var filepath1 = Path.Join(Environment.CurrentDirectory, "example_video.ts");
+        var hlsDownloader = new HlsDownloader();
+        var qualities = await hlsDownloader.GetQualitiesAsync(url);
+
+        await AnsiConsole.Progress().StartAsync(async ctx =>
+        {
+            var progressTask = ctx.AddTask("[cyan]Downloading test video[/]");
+            progressTask.MaxValue = 1;
+
+            await hlsDownloader.DownloadAsync(qualities[0].Stream!, filepath1, null, progressTask);
+        });
+    }
+}
