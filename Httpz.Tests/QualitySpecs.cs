23ea9fc90473f703b4d9a27f7ffb886dafba5e1c
jerrry08 <jeremyyearwood99@gmail.com>
2024-01-12 16:11:42 -0400

    Add tests

--- Httpz.Tests/QualitySpecs.cs
+++ Httpz.Tests/QualitySpecs.cs
@@ -1,22 +1,23 @@
 ﻿using System.Threading.Tasks;
 using FluentAssertions;
-using Httpz.Tests.TestData;
 using Xunit;
 
 namespace Httpz.Tests;
 
 public class QualitySpecs
 {
-    [Fact]
-    public async Task I_can_get_quality_results_from_an_hls_url()
+    [Theory]
+    [InlineData("http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8")]
+    [InlineData("http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8")]
+    public async Task I_can_get_quality_results_from_an_hls_url(string url)
     {
         // Arrange
         var downloader = new HlsDownloader();
 
         // Act
-        var results = await downloader.GetQualitiesAsync(HlsUrls.Url1);
+        var results = await downloader.GetQualitiesAsync(url);
 
         // Assert
-        results.Should().HaveCountGreaterOrEqualTo(1);
+        results.Should().NotBeEmpty();
     }
 }
--- Httpz.Tests/TestData/HlsUrls.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Httpz.Tests.TestData;
-
-internal static class HlsUrls
-{
-    public const string Url1 =
-        "http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8";
-}
